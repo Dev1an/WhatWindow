6c72f1fdb56971da19cfed211bf13c18ba0812a6
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2025-05-10 21:50:23 +0200

    Initial Commit

---
+++ WhatWindow/WhatWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatWindowApp.swift
+//  WhatWindow
+//
+//  Created by Damiaan Dufaux on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
